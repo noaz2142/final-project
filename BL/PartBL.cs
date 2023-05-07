5a22d75d452a139d8ae403692fa44536983be44d
noaz2142 <noaz2142@gmail.com>
2023-05-07 16:08:56 +0300

    first commit

---
+++ BL/PartBL.cs
@@ -0,0 +1,37 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.DbModels;
+
+
+namespace BL
+{
+    public class PartBL
+    {
+        public bool SaveNewPart(DAL.DbModels.PartForDevice p)
+        {
+            try
+            {
+                PartsDal d = new PartsDal();
+                d.Insert(p);
+                return true;
+            }
+            catch {
+                return false;
+            }
+
+        }
+        public object GetParts(string recieverName = "")
+        {
+          /*  DAL.partDAL partDAL = new DAL.partDAL();*/
+
+            return null;
+        }
+
+    }
+}
---
+++ DAL/PartsDal.cs
@@ -0,0 +1,49 @@
+﻿using DAL.DbModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class PartsDal
+    {
+        public bool Insert(PartForDevice d)
+        {
+            try
+            {
+                using (DbModels.SpareDnContext ctx = new())
+                {
+                    ctx.PartForDevices.Add(d);
+                    ctx.SaveChanges();
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+
+            }
+        }
+
+        public bool GetAll(PartForDevice d)
+        {
+            try
+            {
+                using (DbModels.SpareDnContext ctx = new())
+                {
+                    ctx.PartForDevices.Add(d);
+                    ctx.SaveChanges();
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+
+            }
+        }
+
+    }
+}
